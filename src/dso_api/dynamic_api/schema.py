9b0a77b94ce347c7a82bf8ed1b3ad809dbfa4b12
vincent <vincentr@live.nl>
2021-06-08 10:16:44 +0200

    adding custom node to relay to allow for id lookups

--- src/dso_api/dynamic_api/schema.py
+++ src/dso_api/dynamic_api/schema.py
@@ -11,6 +11,7 @@ from rest_framework.viewsets import ViewSet
 from schematools.contrib.django.models import DynamicModel
 from schematools.utils import to_snake_case
 
+from dso_api.dynamic_api.filterset import filterset_factory
 from dso_api.dynamic_api.permissions import (
     fetch_scopes_for_model,
     get_permission_key_for_field,
@@ -22,6 +23,20 @@ from dso_api.dynamic_api.permissions import (
 # rest_framework_gis/fields.py
 def convert_json_field_to_string(field, registry=None):
     return graphene.JSONString()
+
+
+class CustomNode(relay.Node):
+    class Meta:
+        name = "Node"
+
+    @staticmethod
+    def to_global_id(type_, id):
+        return f"{id}"
+
+    @staticmethod
+    def get_node_from_global_id(info, global_id, only_type=None):
+        model = info.return_type.graphene_type._meta.model
+        return model.objects.get(id=global_id)
 
 
 def field_based_auth(
@@ -50,7 +65,8 @@ def field_based_auth(
     active_profiles = request.auth_profile.get_active_profiles(
         model.get_dataset_id(), model.get_table_id()
     )
-    print(active_profiles)
+
+    print(info.field_name)
 
     field_name = to_snake_case(info.field_name)
 
@@ -91,11 +107,11 @@ def create_schema(viewsets: List[ViewSet]) -> graphene.Schema:
 
         field_names = viewset.get_serializer_class(viewset).Meta.fields
         model = viewset.get_serializer_class(viewset).Meta.model
-        print(field_names)
         if "name" in field_names or "class" in field_names:
             continue
 
-        # filterset = filterset_factory(model)
+        filterset = filterset_factory(model)
+        print(filterset)
         # some error with csv filterset? Not working with graphene
         # print(model, field_names)
 
@@ -109,7 +125,7 @@ def create_schema(viewsets: List[ViewSet]) -> graphene.Schema:
                 # add actual filterable fields here.
                 "filter_fields": {"id": ["exact"]} if "id" in field_names else {},
                 # "filterset_class": filterset,
-                "interfaces": (relay.Node,),
+                "interfaces": (CustomNode,),
                 "extra": {
                     "dataset_id": model.get_dataset_id(),
                     "table_id": model.get_table_id(),
@@ -117,7 +133,27 @@ def create_schema(viewsets: List[ViewSet]) -> graphene.Schema:
             },
         )
 
+        if model.get_table_id() == "panden":
+            print(str(model))
+            print(model.get_table_id())
+            print(model.get_dataset_id())
+
+            print(
+                [
+                    f"resolve_{to_snake_case(field_name)}"
+                    for field_name in field_names
+                    if field_name != "_links"
+                ]
+            )
+
+        # convert fieldnames to snake case
+        field_names = [
+            to_snake_case(field_name) if field_name != "_links" else field_name
+            for field_name in field_names
+        ]
+
         class_id = f"{model.get_dataset_id()}_{model.get_table_id()}"
+        print(model, class_id)
         # https://medium.com/@jefmoura/how-to-secure-graphql-in-drf-without-duplicating-code-5a033599db17
         created_class = type(
             class_id,
@@ -126,12 +162,7 @@ def create_schema(viewsets: List[ViewSet]) -> graphene.Schema:
                 "Meta": meta,
                 # add table authorization
                 # TODO
-                # table name
-                f"resolve_{model}": lambda root, info: field_based_auth(
-                    root,
-                    info,
-                    model,
-                ),
+                # table auth?
                 # adding field based authorization
                 **{
                     f"resolve_{field_name}": lambda root, info: field_based_auth(
@@ -143,18 +174,27 @@ def create_schema(viewsets: List[ViewSet]) -> graphene.Schema:
                 },
             },
         )
+        if class_id == "bag_panden":
+            print(dir(created_class))
+            print()
 
         record_schemas[class_id] = created_class
 
     # create Query in similar way
     fields = {}
     for key, rec in record_schemas.items():
-        fields[key] = relay.Node.Field(rec)
-        fields[f"all_{key}"] = DjangoFilterConnectionField(rec)
+        fields[key] = CustomNode.Field(rec)
+        fields[f"all_{key}"] = DjangoFilterConnectionField(
+            rec,  # filterset_class=filterset_factory(rec._meta.model)
+        )
+        # ssert isinstance(filter_field, BaseCSVFilter) -> error
+
+        fields[f"resolve_{key}"] = lambda root, info, **kwargs: print("hi")
 
         # according to relay spec we need to add root field node
-        fields["node"] = relay.Node.Field()
-        Query = type("Query", (graphene.ObjectType,), fields)
+        fields["node"] = CustomNode.Field()
+
+    Query = type("Query", (graphene.ObjectType,), fields)
 
     schema = graphene.Schema(
         query=Query,
